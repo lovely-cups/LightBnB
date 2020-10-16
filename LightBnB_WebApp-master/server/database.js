09fd63c7ff383ae86ac8452e7e2e52c6e41f6bed
‘Charles <charlesthompson.2693@gmail.com>
2020-10-16 00:14:24 +0000

    update for all reservations function

--- LightBnB_WebApp-master/server/database.js
+++ LightBnB_WebApp-master/server/database.js
@@ -72,7 +72,19 @@ exports.addUser = addUser;
  * @return {Promise<[{}]>} A promise to the reservations.
  */
 const getAllReservations = function(guest_id, limit = 10) {
-  return getAllProperties(null, 2);
+  return pool.query(`
+SELECT reservations.*, properties.*, AVG(rating) as average_rating
+FROM reservations
+JOIN properties ON properties.id = reservations.property_id
+JOIN property_reviews ON property_reviews.property_id = reservations.property_id
+WHERE reservations.guest_id = $1
+GROUP BY reservations.id, properties.id, properties.title, cost_per_night
+ORDER BY start_date DESC
+LIMIT $2;`,
+    [guest_id, limit]
+  )
+  .then(res => res.rows);
+};
 }
 exports.getAllReservations = getAllReservations;
 
